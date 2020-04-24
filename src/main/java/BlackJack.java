5498f3953e075f5a2ea9d2ff602a50ea52848a90
Hannes Luidalepp <luidalepp@quretec.com>
2020-04-25 01:14:26 +0300

    täiendused

--- src/main/java/BlackJack.java
+++ src/main/java/BlackJack.java
@@ -1,11 +1,13 @@
 import javafx.application.Application;
 import javafx.event.EventHandler;
+import javafx.geometry.Insets;
 import javafx.geometry.Orientation;
 import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.Labeled;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -14,9 +16,7 @@ import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
-
-
-
+import java.io.BufferedWriter;
 
 
 public class BlackJack extends Application {
@@ -26,6 +26,7 @@ public class BlackJack extends Application {
         FlowPane juur = new FlowPane(); // luuakse juur
         juur.setOrientation(Orientation.VERTICAL);
         juur.setAlignment(Pos.TOP_CENTER);
+        juur.setVgap(20);
 
         // Taust
         BackgroundImage myBI= new BackgroundImage(new Image("Untitled.png"),
@@ -52,13 +53,63 @@ public class BlackJack extends Application {
         juur.getChildren().add(rida1);
 
 
-
-        Scene stseen1 = new Scene(juur, 535, 635, Color.RED);  // luuakse stseen
+        Scene stseen1 = new Scene(juur, 600, 660, Color.RED);  // luuakse stseen
         peaLava.setTitle("Black Jack");  // lava tiitelribale pannakse tekst
         peaLava.setScene(stseen1);  // lavale lisatakse stseen
         peaLava.show();
 
-        //teine rida
+
+        // Teine rida
+        VBox rida2 = new VBox();
+        rida2.setSpacing(10);
+        rida2.setVisible(false); // Peidab alguses
+        VBox.setMargin(rida2, new Insets(50, 0, 50, 50));
+        Label kasutajanimi = new Label("Kasutaja nimi: ");
+        Label summa = new Label("Raha: ");
+        kasutajanimi.setStyle("-fx-font-weight: bold");
+        summa.setStyle("-fx-font-weight: bold");
+        rida2.getChildren().addAll(kasutajanimi,summa);
+        juur.getChildren().add(rida2);
+        //kasutajanimiSisestus.clear();
+        //leiaKasutaja.setOnMouseClicked(e -> leiaKasutaja.setDisable(false));
+
+
+        // Kolmas rida
+
+        HBox rida3 = new HBox();
+        rida3.setSpacing(10);
+        rida3.setVisible(false); // Peidab alguses
+        Label panus = new Label("Panus:");
+        panus.setStyle("-fx-font-weight: bold");
+
+        TextField panuseSisestus = new TextField();
+
+        Button uusM2ng = new Button("Alusta uut mängu");
+        rida3.getChildren().addAll(panus,panuseSisestus,uusM2ng);
+        juur.getChildren().add(rida3);
+
+        // Neljas plokk
+
+        FlowPane m2nguplokk = new FlowPane();
+        m2nguplokk.setOrientation(Orientation.VERTICAL);
+        m2nguplokk.setAlignment(Pos.TOP_CENTER);
+        m2nguplokk.setVgap(20);
+        m2nguplokk.setVisible(false);
+
+        Label diiler = new Label("Diiler:");
+        HBox diileriKaardid = new HBox();
+        Label mangija = new Label("Mängija:");
+        HBox mangijaKaardid = new HBox();
+        HBox nupud = new HBox();
+        nupud.setVisible(false);
+
+        Button v6ta = new Button("Võta juurde");
+        Button eiV6ta = new Button("Ei võta juurde");
+        nupud.getChildren().addAll(v6ta, eiV6ta);
+
+        m2nguplokk.getChildren().addAll(diiler, diileriKaardid, mangija,mangijaKaardid,nupud);
+        juur.getChildren().add(m2nguplokk);
+
         leiaKasutaja.setOnMouseClicked(new EventHandler<MouseEvent>() {
             @Override
             public void handle(MouseEvent mouseEvent) {
@@ -81,16 +132,20 @@ public class BlackJack extends Application {
 
                 }
                 else if(!kasutajanimiSisestus.getText().isEmpty()){
-                    HBox rida2 = new HBox();
-                    rida2.setSpacing(10);
-                    Label kasutajanimi = new Label("Kasutaja nimi: " + kasutajanimiSisestus.getText());
-                    Label summa = new Label("Summa: ");
-                    kasutajanimi.setStyle("-fx-font-weight: bold");
-                    summa.setStyle("-fx-font-weight: bold");
-                    rida2.getChildren().addAll(kasutajanimi,summa);
-                    juur.getChildren().add(rida2);
+
+                    // SIIN ON VAJA VÄLJA KUTSUDA KLASS MIS LOEB FAILIST KASUTAJA ANDMED (SUMMA) VÕI KUI KASUTAJAT
+                    // EI OLE SIIS TEKITAB UUE.
+
+                    kasutajanimi.setText("Kasutaja nimi: " + kasutajanimiSisestus.getText());
                     kasutajanimiSisestus.clear();
+                    summa.setText("Raha: 20");
+
+                    //leiaKasutaja.setDisable(true);
                     leiaKasutaja.setOnMouseClicked(e -> leiaKasutaja.setDisable(false));
+
+                    // Näita sektsioone
+                    rida2.setVisible(true);
+                    rida3.setVisible(true);
                 }
             }
         });
@@ -116,6 +171,44 @@ public class BlackJack extends Application {
                         peaLava.show();
                     }
                 });
+            }
+        });
+
+        uusM2ng.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                if(panuseSisestus.getText().isEmpty()){
+                    FlowPane seletus = new FlowPane();
+                    Label seletus1 = new Label("Pead sisestama panuse!");
+                    Button tagasi = new Button("Tagasi");
+                    seletus.getChildren().addAll(seletus1,tagasi);
+                    Scene error = new Scene(seletus,200,200,Color.SNOW);
+                    peaLava.setScene(error);
+                    peaLava.show();
+
+                    tagasi.setOnMouseClicked(new EventHandler<MouseEvent>() {
+                        @Override
+                        public void handle(MouseEvent mouseEvent) {
+                            peaLava.setScene(stseen1);
+                            peaLava.show();
+                        }
+                    });
+
+                }
+                else if(!panuseSisestus.getText().isEmpty()){
+
+                    // SIIN ON VAJA VÄLJA KUTSUDA KLASS MIS ALUSTAB MÄNGU
+
+                    panuseSisestus.setDisable(true);
+                    panuseSisestus.setStyle("-fx-opacity: 1;");
+                    //peida nupp ise
+                    Button source = (Button) mouseEvent.getSource();
+                    source.setVisible(false);
+
+                    // Näita sektsioone
+                    m2nguplokk.setVisible(true);
+
+                }
             }
         });
 
