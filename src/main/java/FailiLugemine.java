1b5f6f6942bacd5bcdf2583f26c297b38beb6b0c
JoonasAJ <joonad56@gmail.com>
2020-04-26 23:33:59 +0300

    Muutsin klassi FailiLugemine

--- src/main/java/FailiLugemine.java
+++ src/main/java/FailiLugemine.java
@@ -1,15 +1,19 @@
 import java.io.*;
+import java.util.List;
 
 public class FailiLugemine {
 
     public double loeFailist(String kasutajanimi) throws Exception{
         double summa = 20.0;
-        if(new File("Mängijad.txt").exists()){
-            try(BufferedReader lugeja = new BufferedReader(new InputStreamReader(new FileInputStream(new File("Mängijad.txt"))))){
+        boolean sisaldab_Kasutajat = false;
+        File fail = new File("Mängijad.txt");
+        if(fail.exists()){
+            try(BufferedReader lugeja = new BufferedReader(new InputStreamReader(new FileInputStream(fail)))){
                 String rida = lugeja.readLine();
                 while(rida != null){
                     String[] tykid = rida.split(" ");
                     if(tykid[0].equals(kasutajanimi)){
+                        sisaldab_Kasutajat = true;
                         summa = Double.parseDouble(tykid[1]);
                         break;
                     }
@@ -17,9 +21,9 @@ public class FailiLugemine {
                 }
             }
         }
-        else if(!new File("Mängijad.txt").exists()){
-            try(BufferedWriter kirjutaja = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(new File("Mängijad.txt"))))){
-                kirjutaja.write(kasutajanimi + " " + summa);
+        if(!sisaldab_Kasutajat){
+            try(PrintWriter kirjutaja = new PrintWriter(new BufferedWriter(new FileWriter(fail,true)))){
+                kirjutaja.println(kasutajanimi + " " + summa);
             }
         }
         return summa;
@@ -28,17 +32,30 @@ public class FailiLugemine {
 
 
     public void uuendaAndmeid(double summa,String kasutajanimi) throws Exception{
-        try(BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(new File("Mängijad.txt"))))) {
-            String rida = br.readLine();
-            while(rida != null){
+        try{
+            BufferedReader fail = new BufferedReader(new FileReader("Mängijad.txt"));
+            StringBuffer sisend = new StringBuffer();
+            String rida;
+
+            while((rida = fail.readLine()) != null){
                 if(rida.contains(kasutajanimi)){
-                    String rahakott = String.valueOf(new Mängur().getRahakott());
-                    try(BufferedWriter kirjutaja = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(new File("Mängijad.txt"))))){
-                        kirjutaja.write(kasutajanimi + " " + rahakott);
-                    }
+                    String uusrida = kasutajanimi + " " + summa;
+                    sisend.append(uusrida);
+                    sisend.append("\n");
                 }
-                rida = br.readLine();
+                if (!rida.contains(kasutajanimi)){
+                    sisend.append(rida);
+                    sisend.append("\n");
+                }
             }
+            fail.close();
+
+            FileOutputStream kirjuta = new FileOutputStream("Mängijad.txt");
+            kirjuta.write(sisend.toString().getBytes());
+            kirjuta.close();
+
+        } catch (Exception e){
+            e.printStackTrace();
         }
     }
 }
